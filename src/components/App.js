77170e3db3f0c9bf86c94bb5ced108a903056283
Sherin2020 <62859010+Sherin2020@users.noreply.github.com>
2021-07-09 13:44:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ class App extends Component {
   render() {
     return (
       <div className="Clock">
-       <h3 id="time">
+        <h3 id="time">
     {(this.state.date.getHours()>12)?((this.state.date.getHours()%12)+":"+(this.state.date.toLocaleTimeString().split(':').splice(1).join(':'))):
    (((this.state.date.getHours()<10)&&(this.state.date.toLocaleTimeString().split('').splice(1).join('')))||((this.state.date.getHours()>=10) && (this.state.date.toLocaleTimeString())))} {(this.state.date.getHours()>=12)?"PM":"AM"}
    </h3>
