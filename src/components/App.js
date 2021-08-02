bf542856d6f271f6cb338571fbc6f5b41d835679
Sherin2020 <62859010+Sherin2020@users.noreply.github.com>
2021-08-03 00:26:06 +0530

    Update timestring to a less complex one.

--- src/components/App.js
+++ src/components/App.js
@@ -29,9 +29,11 @@ class App extends Component {
     return (
       <div className="Clock">
     <h3 id="time">
-    {(this.state.date.getHours()>12)?((this.state.date.getHours()%12)+":"+(this.state.date.toLocaleTimeString().split(':').splice(1).join(':'))):
-   (((this.state.date.getHours()<10)&&(this.state.date.toLocaleTimeString().split('').splice(1).join('')))||((this.state.date.getHours()>=10) && (this.state.date.toLocaleTimeString())))} {(this.state.date.getHours()>=12)?"PM":"AM"}
-   </h3>
+        {this.state.date.toLocaleTimeString()}
+    {/*{(this.state.date.getHours()>12)?((this.state.date.getHours()%12)+":"+(this.state.date.toLocaleTimeString().split(':').splice(1).join(':'))):
+   (((this.state.date.getHours()<10)&&(this.state.date.toLocaleTimeString().split('').splice(1).join('')))||((this.state.date.getHours()>=10) && (this.state.date.toLocaleTimeString())))} {(this.state.date.getHours()>=12)?"PM":"AM"} */}
+    
+       </h3>
       </div>
     );
   }
